e8906d54353c9b3f16fe5d54068c665c1bc04d33
Immora <kasia_pi1@wp.pl>
2016-04-02 14:54:38 +0200

    saving and erasing query from file

--- Program.cs
+++ Program.cs
@@ -1,62 +1,93 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Net;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace ChomikNotifier
 {
     public class Program
     {
-        static string url = "http://chomikuj.pl";
-        
+        static readonly string url = "http://chomikuj.pl";
+
+        static readonly string fileName = "searches.txt";
+
         public static void Main(string[] args)
         {
             //todo: pobieranie listy ostatnio wyszukiwanych odcinków
-            
-            StringBuilder sb = new StringBuilder();
-            
-            Console.WriteLine("Gimme tv show and episode number in format: TVSHOWNAME SxxExx");
-                       
-            string episodeToSearch = Console.ReadLine() + " 720p";
-                       
-            SendRequest(episodeToSearch).Wait();
+
+            Console.WriteLine("Search for:");
+
+            string episodeToSearch = Console.ReadLine();
+
+            SendRequest(episodeToSearch + " 720p").Wait();
         }
-        
-        private static async Task SendRequest(string fileName)
+
+        private static async Task SendRequest(string searchQuery)
         {
             using (var client = new HttpClient())
             {
                 try
                 {
                     client.BaseAddress = new Uri(url);
-                    
+
                     var content = new FormUrlEncodedContent(new[]
                     {
-                        new KeyValuePair<string, string>("filename", fileName)
+                        new KeyValuePair<string, string>("filename", searchQuery)
                     });
-                    
+
                     var response = await client.PostAsync("/action/SearchFiles", content);
                     response.EnsureSuccessStatusCode();
-                    
+
                     var stringResponse = await response.Content.ReadAsStringAsync();
-                    
-                    
+
                     if (stringResponse.Contains("Nie znaleziono pliku"))
                     {
-                        System.Console.WriteLine("Nie ma jeszcze odcinka :(");
+                        SaveToFile(searchQuery);
+                        System.Console.WriteLine("File not found :(");
                     }
                     else
                     {
-                        System.Console.WriteLine("JEST ODCINEK!!!!!!!!! TODO: DORÓB ŚCIĄGANIE/OTWIERANIE STRONY/WYSYŁKĘ MAILA :P");
+                        EraseFromFile(searchQuery);
+                        System.Console.WriteLine("Found!");
                     }
-                    
+
                 }
-                catch(HttpRequestException e)
+                catch (HttpRequestException e)
                 {
                     System.Console.WriteLine($"Error message: {e.Message}");
+                }
+            }
+        }
+
+        private static void SaveToFile(string searchQuery)
+        {
+            var lines = File.ReadAllLines(fileName);
+
+            foreach (var line in lines)
+            {
+                if (line.Equals(searchQuery))
+                {
+                    return;
+                }
+            }
+
+            File.AppendAllLines(fileName, new List<string>() { searchQuery });
+        }
+
+        private static void EraseFromFile(string searchQuery)
+        {
+            List<string> lines = new List<string>(File.ReadAllLines(fileName));
+
+            foreach (var line in lines)
+            {
+                if (line.Equals(searchQuery))
+                {
+                    lines.Remove(line);
+
+                    File.WriteAllLines(fileName, lines.ToArray());
+
+                    return;
                 }
             }
         }
